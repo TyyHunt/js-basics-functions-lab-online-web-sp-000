730c890369799325b88d6ad504fa7a476eb97879
Tyler Hunt <tyhunt04@gmail.com>
2019-06-05 03:58:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(someValue) {
-  let distance = Math.abs(someValue - 42)
-  return distance
+  let distance = Math.abs(someValue - 42);
+  return distance;
 }
 
 function distanceFromHqInFeet(someValue) {
-  let distance = distanceFromHqInBlocks(someValue) * 264
-  return distance
+  let distance = distanceFromHqInBlocks(someValue) * 264;
+  return distance;
 }
 
 function distanceTravelledInFeet(value1, value2) {
-  let distanceInBlocks = Math.abs(value1 - value2)
-  let distanceInFeet = distanceInBlocks * 264
-  return distanceInFeet
+  let distanceInBlocks = Math.abs(value1 - value2);
+  let distanceInFeet = distanceInBlocks * 264;
+  return distanceInFeet;
+}
+
+function calculatesFarePrice(start, destination) {
+  let distance = distanceTravelledInFeet(start, destination);
+
+  let price;
+
+  switch (distance) {
+    case distance >= 2500:
+        price = 'sorry we cant do that';
+    case sitance <= 400:
+        price = 0;
+        canEnlist = true;
+    case age >= 16:
+        canWork = true;
+}
 }
