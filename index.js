098618ba7ddf75eb3ca85313f91989cc939b274c
Tyler Hunt <tyhunt04@gmail.com>
2019-06-05 03:44:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function distanceFromHqInBlocks(someValue) {
   let distance = Math.abs(someValue - 42)
   return distance
 }
+
+function distanceFromHqInFeet(someValue) {
+  let distance = distanceFromHqInBlocks(someValue) * 264
+  return distance
+}
